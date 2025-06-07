d06cafb0c1346e2b4360b0a1c7e21a3dff58d6a4
eycis <eycisxxx@gmail.com>
2025-06-07 23:44:12 +0200

    images debug

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,37 +11,37 @@ const Projects = () => {
             <div data-aos = "fade-up">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/02.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/02.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>
             </div>
             <div data-aos = "fade-up" data-aos-delay="300">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/03.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/03.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>       
             </div>
             <div data-aos = "fade-up">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/04.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/04.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>
             </div>
             <div data-aos = "fade-up" data-aos-delay="300">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/07.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/07.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>
             </div>
             <div data-aos = "fade-up" data-aos-delay="300">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/05.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/05.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>
             </div>
             <div data-aos = "fade-up" data-aos-delay="300">
                 <div className='transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative
                 w-[100%] h-[200px] md:h-[300px]'>
-                    <Image src="/images/06.PNG" alt='portfolio' layout='fill' className='object-contain'/>
+                    <Image src="/images/06.png" alt='portfolio' layout='fill' className='object-contain'/>
                 </div>
             </div>
         </div>
